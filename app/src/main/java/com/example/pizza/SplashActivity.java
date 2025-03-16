896411bb93e4392174ec9fa52592981dc60d5b33
man-man <102537717+OUALID-12@users.noreply.github.com>
2025-03-16 12:03:47 +0000

    first commit

---
+++ app/src/main/java/com/example/pizza/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.pizza;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        Thread t1 = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                    Intent intent = new Intent(SplashActivity.this, ListPizzaActivity.class);
+                    startActivity(intent);
+                    SplashActivity.this.finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        t1.start();
+    }
+}
